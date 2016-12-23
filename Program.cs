4e4b8383f45d8f264014287d9481c8d149423f86
NaveenPulamolu <naveenp992@gmail.com>
2016-12-23 13:24:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOptrEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            Console.WriteLine("Enter the First number to compare");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number to compare");
+            j = Convert.ToInt32(Console.ReadLine());
+            String result = i > j ? "First number is greater than Second number"
+                                : i < j ? "First number is less than Second number"
+                                    : i == j ? "First number is equal to Second number" : "No result";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
